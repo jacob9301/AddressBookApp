e6adb1407635ce86e4e43ee27fc08357392b370d
jacob9301 <jacob.luc.bushby@gmail.com>
2023-08-25 13:18:02 +0100

    feat: add contactrepository interface

---
+++ AddressBookApp/Interfaces/IContactRepository.cs
@@ -0,0 +1,10 @@
+﻿using AddressBookApp.Models;
+
+namespace AddressBookApp.Interfaces
+{
+    public interface IContactRepository
+    {
+        List<Contact> GetContacts();
+        void WriteContacts();
+    }
+}
