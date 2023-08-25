f3df089631fc277c73a3631caa31a36b7d3f0fc1
jacob9301 <jacob.luc.bushby@gmail.com>
2023-08-25 13:21:32 +0100

    feat: add contactrepository class

---
+++ AddressBookApp/Repository/ContactRepository.cs
@@ -0,0 +1,24 @@
+﻿using AddressBookApp.Interfaces;
+using AddressBookApp.Models;
+
+namespace AddressBookApp.Repository
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly string _path;
+        public ContactRepository(string path)
+        {
+            _path = path;
+        }
+
+        public List<Contact> GetContacts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteContacts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
