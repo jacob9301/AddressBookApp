2897fb2deb927c2c87e708c744cc05157dd292b5
jacob9301 <jacob.luc.bushby@gmail.com>
2023-08-25 12:30:35 +0100

    feat: add contact controller

---
+++ AddressBookApp/Controllers/ContactController.cs
@@ -0,0 +1,42 @@
+﻿using AddressBookApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AddressBookApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController : ControllerBase
+    {
+        [HttpPost("create-contact")]
+        public async Task<IActionResult> CreateContact()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("get-all-contacts")]
+        public async Task<IActionResult> GetAllContacts()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("get-contact-by-id")]
+        public async Task<IActionResult> GetContactById()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("update-contact")]
+        public async Task<IActionResult> UpdateContact()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("delete-contact-by-id")]
+        public async Task<IActionResult> DeleteContactById()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
